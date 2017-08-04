c5a7f00663ca67680383ca5b8407ae89476bc6a8
Cenyol <cenyol@163.com>
2017-08-04 21:19:01 +0800

    --

--- src/main/java/com/cenyol/example/controller/MainController.java
+++ src/main/java/com/cenyol/example/controller/MainController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.lang.reflect.Method;
 import java.util.List;
 
 /**
--- src/main/java/com/cenyol/example/model/UserEntity.java
+++ src/main/java/com/cenyol/example/model/UserEntity.java
@@ -7,7 +7,7 @@ import java.util.Collection;
  * Created by sjj on 2015/10/24 0024.
  */
 @Entity
-@Table(name = "user", schema = "", catalog = "test")
+@Table(name = "spring_mvc_user", schema = "", catalog = "test")
 public class UserEntity {
     private int id;
     private String firstName;
