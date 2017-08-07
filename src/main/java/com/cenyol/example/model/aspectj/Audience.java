d85f060084e481c3df67d22b54f5558649932cd5
Cenyol <cenyol@163.com>
2017-08-07 15:10:23 +0800

    add the Around function in the class of aspectj

--- src/main/java/com/cenyol/example/model/aspectj/Audience.java
+++ src/main/java/com/cenyol/example/model/aspectj/Audience.java
@@ -1,5 +1,6 @@
 package com.cenyol.example.model.aspectj;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -15,24 +16,37 @@ public class Audience {
 
     @Pointcut("execution(* com.cenyol.example.model.aspectj.Performance.perform(..))")
     public void performance() {}
+//
+//    @Before("execution(* com.cenyol.example.model.aspectj.Performance.perform(..))")
+//    public void silenceCellPhones() {
+//        System.out.println("Silencing cell phone");
+//    }
+//
+//    @Before("performance()")
+//    public void takeSeats() {
+//        System.out.println("Taking seats");
+//    }
+//
+//    @AfterReturning("performance()")
+//    public void applause() {
+//        System.out.println("CLAP CLAP CLAP !!!");
+//    }
+//
+//    @AfterThrowing("performance()")
+//    public void demandRefund() {
+//        System.out.println("Demanding a refund");
+//    }
 
-    @Before("execution(* com.cenyol.example.model.aspectj.Performance.perform(..))")
-    public void silenceCellPhones() {
+    @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint joinPoint) {
         System.out.println("Silencing cell phone");
-    }
-
-    @Before("performance()")
-    public void takeSeats() {
         System.out.println("Taking seats");
+        try {
+            joinPoint.proceed();
+            System.out.println("CLAP CLAP CLAP !!!");
+        } catch (Throwable throwable) {
+            System.out.println("Demanding a refund");
+        }
     }
 
-    @AfterReturning("performance()")
-    public void applause() {
-        System.out.println("CLAP CLAP CLAP !!!");
-    }
-
-    @AfterThrowing("performance()")
-    public void demandRefund() {
-        System.out.println("Demanding a refund");
-    }
 }
