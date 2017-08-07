8ca410e50771fa2b8d03761aafa0a537b7d5113b
Cenyol <cenyol@163.com>
2017-08-07 10:01:33 +0800

    添加java config 装配bean;

--- src/main/java/com/cenyol/example/model/autowire/CDPlayerConfig.java
+++ src/main/java/com/cenyol/example/model/autowire/CDPlayerConfig.java
@@ -1,12 +1,12 @@
 package com.cenyol.example.model.autowire;
 
-import com.cenyol.example.model.autowire.interfaces.CompactDisc;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Cenyol mail: mr.cenyol@gmail.com
  * @date 04/08/2017 21:31
  */
-@ComponentScan
+// 此处可以指定特定的报名，如果不指定则默认扫描本配置文件所在的包及其子包
+@ComponentScan("com.cenyol.example.model.autowire")
 public class CDPlayerConfig {
 }
